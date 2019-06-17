e04b464eee66201012a5a9121b4584ffac675589
teo <ldptek@gmail.com>
2019-06-17 12:44:37 +0100

    Now with Post component and passing posts

---
+++ vuescrapper/src/components/Post.vue
@@ -0,0 +1,17 @@
+<template>
+    <div><h3>{{post.title}}</h3>
+    <p>{{post.content}}</p></div>
+</template>
+
+<script>
+export default {
+    name: "Post",
+    props: ["post"]
+}
+
+</script>
+
+
+<style scoped>
+
+</style>
--- vuescrapper/src/components/Posts.vue
+++ vuescrapper/src/components/Posts.vue
@@ -3,15 +3,21 @@
 
         <h1>POSTS</h1>
     <div v-bind:key="post.id" v-for="post in posts"> 
-        {{post.title}}
+        <Post v-bind:post="post" />
 
     </div>
     </div>
 </template>
 
 <script>
+
+import Post from './Post.vue'
+
 export default {
     name: "Posts",
+    components: {
+        Post
+    },
     props: ["posts"]
 }
 </script>
