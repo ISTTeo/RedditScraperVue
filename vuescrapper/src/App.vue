00e9e98627aa138f0ac01d9f0bfe7c9186f2622d
teo <ldptek@gmail.com>
2019-06-17 12:40:22 +0100

    Basic showing of post title with sample posts in App

--- vuescrapper/src/App.vue
+++ vuescrapper/src/App.vue
@@ -1,17 +1,65 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header msg="Reddit's Top Scrapper"/>
+    <Posts v-bind:posts="posts"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Posts from './components/Posts.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header,
+    Posts
+  },
+  data() {
+    return {
+      posts: [
+        {
+          author: "user1",
+          title: "title1",
+          content: "content1",
+          link: "link1",
+          id: "id1",
+          pubDate: "pubDate1"
+        },
+        {
+          author: "user2",
+          title: "title2",
+          content: "content2",
+          link: "link2",
+          id: "id2",
+          pubDate: "pubDate2"
+        },
+        {
+          author: "user3",
+          title: "title3",
+          content: "content3",
+          link: "link3",
+          id: "id3",
+          pubDate: "pubDate3"
+        },
+        {
+          author: "user4",
+          title: "title4",
+          content: "content4",
+          link: "link4",
+          id: "id4",
+          pubDate: "pubDate4"
+        }
+        ,{
+          author: "user5",
+          title: "title5",
+          content: "content5",
+          link: "link5",
+          id: "id5",
+          pubDate: "pubDate5"
+        }
+      ]
+    }
   }
 }
 </script>
---
+++ vuescrapper/src/components/Posts.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+
+        <h1>POSTS</h1>
+    <div v-bind:key="post.id" v-for="post in posts"> 
+        {{post.title}}
+
+    </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Posts",
+    props: ["posts"]
+}
+</script>
+
+<style scoped>
+
+</style>
