4c4b24bb0e24a03c4c4d05199cdff0b85ddddad6
teo <ldptek@gmail.com>
2019-06-21 15:10:42 +0100

    before router

--- vuescrapper/src/components/Post.vue
+++ vuescrapper/src/components/Post.vue
@@ -1,16 +1,29 @@
 <template>
   
-    <div>
-        <p>
-          <a class="btn btn-primary" data-toggle="collapse" href="#{{post.id}}" role="button" aria-expanded="false" aria-controls="collapseExample">
-            {{post.title}}
-          </a>
+      <div style="text-align:left;margin-left: 20px;">
+        <h4>{{post.title}}</h4>
+        <div v-if="cleanText.length > 200"> {{ cleanText.substring(0,200) }} <b>. . .</b></div>
+        <div v-if="cleanText.length <= 200"> {{ cleanText }} </div>
+        <hr>
+      </div>
+      <!-- <b-card no-body class="mb-1">
+        <b-card-header v-b-toggle="post.guid" header-tag="header" class="p-1" role="tab">
             
-        </p>
-        <div class="collapse" id="#{{post.id}}">
+            <a >{{post.title}}</a>
+        </b-card-header>
+        <b-collapse  v-bind:id="post.guid"  role="tabpanel">
+            <b-card-body>
+              <div v-if="cleanText.length > 200"> {{ cleanText.substring(0,200) }} <b>. . .</b></div>
+              <div v-if="cleanText.length <= 200"> {{ cleanText }} </div>
+            </b-card-body>
+        </b-collapse>
+      </b-card> -->
+        
+          
+        <!-- <div class="collapse" id="#col">
           <div v-html="post.content" class="card card-body"></div>
-        </div>
-    </div>
+        </div> -->
+   
     
 </template>
 
@@ -21,18 +34,32 @@ export default {
     name: "Post",
     props: ["post"],
     data() {
-    return {
-      comments: [],
-      result: null,
-    }
-  },
+      return {
+        comments: [],
+        cleanText: null
+      }
+    },
+    methods: {
+      strip(html)
+        {
+          var tmp = document.createElement("DIV");
+          tmp.innerHTML = html;
+          return tmp.textContent || tmp.innerText || "";
+        }
+    },
   created: function() {
-  axios.get( "https://api.rss2json.com/v1/api.json?rss_url=" + "http://www.reddit.com/r/" + "entrepreneur" + "/comments/" + this.post.id + "/.xml?limit=" + "5")
-    .then(res => {
-      this.result = res;
-      this.comments = null;
-    } )
-    .catch(err => console.log(err));
+    /* axios.get( "https://api.rss2json.com/v1/api.json?rss_url=" + "http://www.reddit.com/r/" + "entrepreneur" + "/comments/" + this.post.id + "/.xml?limit=" + "5")
+      .then(res => {
+        this.result = res;
+        this.comments = null;
+        
+      } )
+      .catch(err => console.log(err)); */
+    if(this.$props.post.content.indexOf("submitted")> 10) {
+      this.cleanText = this.strip(this.$props.post.content);
+      /* this.result = this.$props.post.content.substring(33,this.$props.post.content.indexOf(" </div>\n<!-- SC_ON -->")) */
+    }
+    
   }
 }
 
