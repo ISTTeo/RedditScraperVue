0566d1217f2bc5c6857be9186ea51635de9881ca
teo <ldptek@gmail.com>
2019-06-21 16:00:00 +0100

    add view

--- vuereddit/src/views/Home.vue
+++ vuereddit/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Meesek" src="../assets/meeseek.png" width="25%">
+    <h1>REDDIT Knowledge Machine</h1>
+    
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
