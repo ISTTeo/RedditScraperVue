1b38a59e1651294d383cb8a4788d2bb534a8ee6f
Teo <ldptek@gmail.com>
2019-06-24 10:50:39 +0100

    Make search look better

--- vuereddit/src/components/SearchSubReddit.vue
+++ vuereddit/src/components/SearchSubReddit.vue
@@ -2,9 +2,9 @@
     <div>
         <form @submit="searchReddit">
             <input type="text" v-model="subreddit" name="subreddit" placeholder="/r/...">
-            <input type="submit" value="Search">
+            <input class="searchBtn" type="submit" value="Search">
         </form>
-        <div>
+        <div style="margin-top:10px;">
             <input id="allBtn" type="button" class="button active" v-on:click="setTime('all')" value="all">
             <input id="yearBtn" type="button" class="button" v-on:click="setTime('year')" value="year">
             <input id="monthBtn" type="button" class="button" v-on:click="setTime('month')" value="month">
@@ -48,8 +48,36 @@ export default {
 </script>
 
 <style>
-    .active {
-        color: red;
+    input[type=text] {
+        width: 30%;
+        box-sizing: border-box;
+        border: 2px solid #ccc;
+        border-radius: 4px;
+        font-size: 16px;
+        background-color: white;
+        background-image: url('../assets/searchicon.png');
+        background-position: 10px 10px; 
+        background-repeat: no-repeat;
+        padding: 12px 20px 12px 40px;
+        margin-right: 1em;
+    }
+    .searchBtn {
+        padding: 5px 8px 5px 8px;
+    }
+    .button {
+        margin: 5px;
+        font-size: 150%;
+        padding-top: 0.2em;
+        padding-bottom: 0.2em;
+        padding-right: 0.5em;
+        padding-left: 0.5em;
+        border-radius: 4px;
+        background-color: #e7e7e7; 
+        color: black;
+    }
+     .active {
+        background-color: #555555;
+        color: white;
     }
 </style>
 
